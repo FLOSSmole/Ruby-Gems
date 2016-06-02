64d9671a63d0bd0931f76f8dba38e721b933dc9c
Megan Squire <megansquire@gmail.com>
2016-06-02 12:43:57 -0400

    Update RubyGemsProjectCollector.py

--- RubyGemsProjectCollector.py
+++ RubyGemsProjectCollector.py
@@ -1,23 +1,36 @@
 # -*- coding: utf-8 -*-
-'''
-This program is free software; you can redistribute it
-and/or modify it under the terms of the Perl Artistic License 2.0.
-Copyright (C) 2015 Megan Squire, Gavan Roth, and Evan Ashwell
-We're working on this at http://flossmole.org - Come help us build 
-an open and accessible repository for data and analyses for open
-source projects.
-If you use this code or data for preparing an academic paper please
-provide a citation to 
-Howison, J., Conklin, M., & Crowston, K. (2006). FLOSSmole: 
-A collaborative repository for FLOSS research data and analyses. 
-International Journal of Information Technology and Web Engineering, 
-1(3), 17–26.
-and
-FLOSSmole (2004-2016) FLOSSmole: a project to provide academic access to data 
-and analyses of open source projects.  Available at http://flossmole.org 
-usage:
-python RubyGemsProjectCollector.py <datasource_id> <password>
-'''
+## This program is free software; you can redistribute it
+## and/or modify it under the same terms as Perl.
+## Please see the Perl Artistic License 2.0.
+## 
+## Copyright (C) 2004-2016 Megan Squire <msquire@elon.edu>
+## Contributors: Evan Ashwell, Gavan Roth
+##
+## We're working on this at http://flossmole.org - Come help us build 
+## an open and accessible repository for data and analyses for open
+## source projects.
+##
+## If you use this code or data for preparing an academic paper please
+## provide a citation to 
+##
+## Howison, J., Conklin, M., & Crowston, K. (2006). FLOSSmole: 
+## A collaborative repository for FLOSS research data and analyses. 
+## International Journal of Information Technology and Web Engineering, 1(3), 17–26.
+##
+## and
+##
+## FLOSSmole: a project to provide research access to 
+## data and analyses of open source projects.  
+## Available at http://flossmole.org 
+##
+################################################################
+# usage:
+# python RubyGemsProjectCollector.py <datasource_id> <status> <password>
+# where <status> is one of RESTART or NEW
+#
+# purpose: 
+# take file from disk and parse out interesting bits, write those to db
+################################################################
 
 import urllib.request
 from bs4 import BeautifulSoup
@@ -26,8 +39,8 @@ import pymysql
 import datetime
 
 datasource_id = sys.argv[1]
-previous_datasource_id= sys.argv[2]
-password = sys.argv[3]
+status        = sys.argv[2]
+password      = sys.argv[3]
 
 urlBase = "https://rubygems.org/gems"
 countT = 1
@@ -35,11 +48,59 @@ count = 0
 page = 1
 i = 0
 
+# establish database connection: ELON
+try:
+    db = pymysql.connect(host='grid6.cs.elon.edu',
+                        database='rubygems',
+                        user='megan',
+                        password=password,
+                        use_unicode=True,
+                        charset='utf8')
+except pymysql.Error as err:
+    print(err)
+else:
+    cursor = db.cursor()
+
+
+# establish database connection: SYR
+try:
+    db1 = pymysql.connect(host='flossdata.syr.edu',
+                        database='rubygems',
+                        user='megan',
+                        password=password,
+                        use_unicode=True,
+                        charset='utf8')
+except pymysql.Error as err:
+    print(err)
+else:
+    cursor1 = db1.cursor()
+   
+
 #Create arrays with letters and their corresponding number of pages
 letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 nums = [None]*26
 
-#Populates the nums array with the curent total number of pages of projects for each letter
+# if this is a re-start, figure out where we left off
+if status == 'RESTART':
+    selectQuery = "SELECT project_name, page_number \
+        FROM rubygems_project_pages \
+        WHERE datasource_id = %s \
+        ORDER BY project_name DESC LIMIT 1;"    
+    cursor.execute(selectQuery, (datasource_id))
+    found    = cursor.fetchone()
+    project  = found[0][0].upper()
+    print(project)
+
+    position = letters.index(project)
+    while position > 0:
+        letters.remove(letters[0])
+        nums.remove(nums[0])
+        position = position-1
+    page = (page + int(found[1]))-1
+    print(page)
+    print(letters)
+    print(nums)
+      
 while i < len(letters):
     p = urllib.request.urlopen(urlBase+"?letter=" + letters[i])
     s = BeautifulSoup(p, "lxml")
@@ -52,55 +113,13 @@ while i < len(letters):
     i = i+1
     countT = 1
 
-# establish database connection: ELON
-try:
-    db = pymysql.connect(host='grid6.cs.elon.edu',
-                                  database='test',
-                                  user='eashwell',
-                                  password=password,
-                                  charset='utf8')
-except pymysql.Error as err:
-    print(err)
-else:
-    cursor = db.cursor()
-    if datasource_id == previous_datasource_id:
-        cursor2= db.cursor()
-        selectQuery= "SELECT `project_name`,`page_number`\
-        FROM `rubygems_project_pages`\
-        WHERE datasource_id = %s \
-        ORDER BY `rubygems_project_pages`.`project_name`  DESC LIMIT 1"
-
-        cursor2.execute(selectQuery, (datasource_id))
-
-        found= cursor2.fetchone()
-        project= found[0][0].upper()
-        position= letters.index(project)
-        while position > 0:
-            letters.remove(letters[0])
-            nums.remove(nums[0])
-            position= position-1
-        page= (page+int(found[0][1]))-1
-# establish database connection: SYR      
-"""
-try:
-    db1 = pymysql.connect(host='flossdata.syr.edu',
-                                  database='rubygems',
-                                  user='megan',
-                                  password=password,
-                                  charset='utf8')
-except pymysql.Error as err:
-    print(err)
-else:
-    cursor1 = db1.cursor()
-"""
-
 #outer while loop used to iterate through the 26 letters
 while count < len(letters):
     letter = letters[count]
     pages = nums[count]
     
-    #This while loop itterates through all the pages listing the projects of the current letter
-    while page < pages+1:
+    #This while loop iterates through all the pages grabbing the projects of the current letter
+    while page <= pages:
         listUrl = urlBase + "?letter=" + letter +"&" 
         listUrl = listUrl + "page=%d" % page
         try:
@@ -116,93 +135,101 @@ while count < len(letters):
             ref = row['href']
             projectName = ref[6:]
             
-            #---- get RSS atom file for each project
-            RSSurl = urlBase + "/" + projectName + "/versions.atom"
-            try:
-                RSSfile = urllib.request.urlopen(RSSurl)
+            # something was wrong with the RSS file for 
+            # some large projects. They are causing a socket error to SYR
+            # if projectName != 'bioroebe':
+            if projectName != 'cookbooks':
+                #---- get RSS atom file for each project
+                RSSurl = urlBase + "/" + projectName + "/versions.atom"
+                print("getting RSS for",projectName)
+                try:
+                    RSSfile = urllib.request.urlopen(RSSurl)
 
-            except urllib.error.URLError as e:
-                print(e.reason)
-            else:
-                RSSsoup = BeautifulSoup(RSSfile, "lxml")
-                RSSpage = RSSsoup.find('feed')
-                RSSstring = str(RSSpage)
+                except urllib.error.URLError as e:
+                    print(e.reason)
+                else:
+                    RSSsoup = BeautifulSoup(RSSfile, "lxml")
+                    RSSpage = RSSsoup.find('feed')
+                    RSSstring = str(RSSpage)
             
-            #---- get HTML base page for each project
-            homePageURL = urlBase + "/" + projectName
-            try:
-                homePageFile = urllib.request.urlopen(homePageURL)
+                #---- get HTML base page for each project
+                homePageURL = urlBase + "/" + projectName
+                print("getting HTML for",projectName)
+                try:
+                    homePageFile = urllib.request.urlopen(homePageURL)
                 
-            except urllib.error.URLError as e:
-                print(e.reason)
+                except urllib.error.URLError as e:
+                    print(e.reason)
          
-            else:
-                homePageSoup = BeautifulSoup(homePageFile, "lxml")
-                homePageString = str(homePageSoup)
+                else:
+                    homePageSoup = BeautifulSoup(homePageFile, "lxml")
+                    homePageString = str(homePageSoup)
 
-            #---- get HTML versions for each project            
-            versionsPageURL = urlBase + "/" + projectName + "/versions"
-            #little confused on how this directly obtains the version
-            try:
-                versionFile = urllib.request.urlopen(versionsPageURL)
-            except urllib.error.URLError as e:
-                print(e.reason)
-            else:
-                versionSoup = BeautifulSoup(versionFile, "lxml")
-                versionString = str(versionSoup)
+                #---- get HTML versions for each project            
+                versionsPageURL = urlBase + "/" + projectName + "/versions"
+                print("getting HTML Versions for",projectName)
+                #little confused on how this directly obtains the version
+                try:
+                    versionFile = urllib.request.urlopen(versionsPageURL)
+                except urllib.error.URLError as e:
+                    print(e.reason)
+                else:
+                    versionSoup = BeautifulSoup(versionFile, "lxml")
+                    versionString = str(versionSoup)
         
-"""        
-            #---- put everything in the database
-            try:
-                cursor.execute("INSERT IGNORE INTO rubygems_project_pages( \
-                    project_name, \
-                    datasource_id, \
-                    rss_file, \
-                    html_file, \
-                    html_versions_file, \
-                    page_number, \
-                    last_updated) \
-                     VALUES (%s,%s,%s,%s,%s,%s,%s)", 
-                     (projectName, 
-                      datasource_id, 
-                      RSSstring, 
-                      homePageString, 
-                      versionString,
-                      page,
-                      datetime.datetime.now()))
-                db.commit()
-            except pymysql.Error as err:
-                print(err)
-                db.rollback()
-
-            try:
-                cursor1.execute("INSERT IGNORE INTO rubygems_project_pages( \
-                    project_name, \
-                    datasource_id, \
-                    rss_file, \
-                    html_file, \
-                    html_versions_file, \
-                    last_updated) \
-                     VALUES (%s,%s,%s,%s,%s,%s)", 
-                     (projectName, 
-                      datasource_id, 
-                      RSSstring, 
-                      homePageString, 
-                      versionString, 
-                      datetime.datetime.now()))
-                db1.commit()
-            except pymysql.Error as err:
-                print(err)
-                db1.rollback()
-        page = page + 1
-    count = count + 1
+            
+                #---- put everything in the database
+                try:
+                    print("inserting",projectName,"into Elon")
+                    cursor.execute("INSERT IGNORE INTO rubygems_project_pages( \
+                        project_name, \
+                        datasource_id, \
+                        page_number, \
+                        rss_file, \
+                        html_file, \
+                        html_versions_file, \
+                        last_updated) \
+                         VALUES (%s,%s,%s,%s,%s,%s,%s)", 
+                         (projectName, 
+                          datasource_id, 
+                          page,
+                          RSSstring, 
+                          homePageString, 
+                          versionString, 
+                          datetime.datetime.now()))
+                    db.commit()
+                except pymysql.Error as err:
+                    print(err)
+                    db.rollback()     
+            
+                try:
+                    print("inserting",projectName,"into Syr")
+                    cursor1.execute("INSERT IGNORE INTO rubygems_project_pages( \
+                        project_name, \
+                        datasource_id, \
+                        page_number, \
+                        rss_file, \
+                        html_file, \
+                        html_versions_file, \
+                        last_updated) \
+                         VALUES (%s,%s,%s,%s,%s,%s,%s)", 
+                         (projectName, 
+                          datasource_id, 
+                          page,
+                          RSSstring, 
+                          homePageString, 
+                          versionString, 
+                          datetime.datetime.now()))
+                    db1.commit()
+                except pymysql.Error as err:
+                    print(err)
+                    db1.rollback()
+        page += 1
+    count += 1
     page = 1
-"""
 
 cursor.close()
-cursor2.close()
 db.close() 
-"""
+
 cursor1.close()
-db1.close()
-"""
+db1.close()  
